01edf6eec2c84016d5be507186ef58099a53b4ea
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2024-06-03 20:02:55 -0300

    pok

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -1,9 +1,48 @@
 import Home from './pages/Home'
 //import './App.css'
+import style from 'styled-components'
+import {motion} from "framer-motion"
+import { useState, useEffect } from 'react'
+
+
+
+const Spinner = style(motion.div)`
+width:50px;
+height:50px;
+border: 4px solid black;
+border-top: 4px solid red;
+border-radius: 50%;
+display: inline-block;
+`;
+
+const Container =style.div`display:flex;
+width: 100vw;
+align-items: center;
+justify-content: center;
+height:100vh;
+`
 
 function App() {
+  const [carregando, setCarregando] = useState(true);
+  useEffect(()=>{
+   const tempo = setTimeout(()=>{
+     setCarregando(false);
+   }, 5000)
+   return () => clearTimeout(tempo);
+  },[])
+
   return (
-    <Home />
+    <>
+    <Container>
+     {carregando ?(
+    <Spinner animate = {{rotate: 360}}
+    transition={{duration: 1, repeat: Infinity, ease: 'linear'}}>
+    </Spinner>
+    ):(
+     <Home/>
+    )} 
+    </Container>
+   </>
   )
 }
 
